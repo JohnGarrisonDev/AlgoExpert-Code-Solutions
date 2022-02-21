51cec83ed9a5151fa7b70d247efcf0cd1579a02f
= <johngarrison1996@gmail.com>
2022-02-21 17:30:25 -0600

    initial commit

---
+++ Arrays/ValidateSubsequence.js
@@ -0,0 +1,23 @@
+/*Validate Sequence
+https://www.algoexpert.io/questions/Validate%20Subsequence
+*/
+//Solution
+function isValidSubsequence(array, sequence) {
+
+	let count = 0;
+	
+	for(let i = 0; i<array.length; i++){
+		if (array[i] == sequence[count]){
+		count++;
+	}
+}
+	if(count == sequence.length){
+		return true; 
+	}
+	else{
+		return false; 
+	}
+}
+
+// Do not edit the line below.
+exports.isValidSubsequence = isValidSubsequence;
